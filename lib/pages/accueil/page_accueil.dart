a67bb4f45dda522f4c46559569a0bd324bd22c09
Florian PETIOT <florian.petiot44@gmail.com>
2024-02-12 23:17:24 +0100

    amélioration du démarage et stop du scan bluetooth

--- lib/pages/accueil/page_accueil.dart
+++ lib/pages/accueil/page_accueil.dart
@@ -1,9 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:polyleaks/bluetooth/bluetooth_manager.dart';
 import 'package:polyleaks/pages/accueil/capteur_slot.dart';
 
 
-class PageAccueil extends StatelessWidget {
+class PageAccueil extends StatefulWidget {
   const PageAccueil({super.key});
+
+  @override
+  State<PageAccueil> createState() => _PageAccueilState();
+}
+
+class _PageAccueilState extends State<PageAccueil> {
+
+  @override
+  void initState() {
+    super.initState();
+    BluetoothManager().scanForDevices(context);
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    BluetoothManager().stopScan();
+  }
 
   @override
   Widget build(BuildContext context) {
