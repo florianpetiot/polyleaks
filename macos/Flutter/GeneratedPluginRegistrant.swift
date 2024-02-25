2d8f7ef49567cc8bb354a4b23f0845d584c4b829
Florian PETIOT <florian.petiot44@gmail.com>
2024-02-25 19:15:21 +0100

    initialisation de l'animation via River sur la page d'acceuil, river n'est pas collaboratif gratuitement donc je m'en occupe

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -8,9 +8,11 @@ import Foundation
 import flutter_blue_plus
 import isar_flutter_libs
 import path_provider_foundation
+import rive_common
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FlutterBluePlusPlugin.register(with: registry.registrar(forPlugin: "FlutterBluePlusPlugin"))
   IsarFlutterLibsPlugin.register(with: registry.registrar(forPlugin: "IsarFlutterLibsPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
+  RivePlugin.register(with: registry.registrar(forPlugin: "RivePlugin"))
 }
